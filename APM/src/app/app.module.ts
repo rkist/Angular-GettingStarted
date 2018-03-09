b9a18cca855498da58d83ae0f3d6926a204689fd
Raul Kist <Raul.Kist@cegal.com>
2018-03-09 16:21:44 +0100

    module

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: 
+  [
+    AppComponent, 
+    ProductListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
