ce4138b9670e1888977d46e5823473851df6f3b6
Raul Kist <Raul.Kist@cegal.com>
2018-03-14 16:07:48 +0100

    incremental

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { ErrorObservable } from "rxjs/observable/ErrorObservable";
 
 @Injectable()
 export class ProductService
@@ -14,6 +17,14 @@ export class ProductService
 
     getProducts(): Observable<IProduct[]>
     {
-        return this._http.get<IProduct[]>(this._productUrl);
+        return this._http.get<IProduct[]>(this._productUrl)
+        .do(data => console.log('All: ' + JSON.stringify(data)))
+        .catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse) : ErrorObservable
+    {
+        console.log(err.name + ' : ' + err.message)
+        return Observable.throw(err.message);
     }
 }
