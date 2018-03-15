9d06b28369eb0ef12b1595cda7f4d154f9d15fca
Raul Kist <Raul.Kist@cegal.com>
2018-03-15 15:58:48 +0100

    feature module

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,50 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
-import { StarComponent } from './shared/start.component';
-import { ProductService } from './products/product.service';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductGuardService } from './products/product-guard.service';
-
-
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: 
   [
     AppComponent, 
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,   
     WelcomeComponent 
   ],
 
   imports: 
   [
     BrowserModule, 
-    FormsModule, 
     HttpClientModule, 
     RouterModule.forRoot
     ([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductGuardService], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full'},
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
-  ],
-
-  providers: 
-  [
-    ProductService,
-    ProductGuardService
+    ]), 
+    ProductModule
   ],
 
   bootstrap: 
